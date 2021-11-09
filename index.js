fc031c656c9bbfb7ea473b0d48a26f5db775f5e8
iamsaikatrahman <saikatrahmanoffical@gmail.com>
2021-11-09 16:38:56 +0600

    docotrs portal server

---
+++ index.js
@@ -0,0 +1,121 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const admin = require("firebase-admin");
+const { MongoClient } = require("mongodb");
+const ObjectId = require("mongodb").ObjectId;
+const app = express();
+const port = process.env.PORT || 5000;
+
+// const serviceAccount = require("./doctors-portal-firebase-adminsdk.json");
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
+app.use(express.json());
+app.use(cors());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jr39v.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+async function verifyToken(req, res, next) {
+  if (req.headers?.authorization?.startsWith("Bearer ")) {
+    const idToken = req.headers.authorization.split(" ")[1];
+    try {
+      const decodeUser = await admin.auth().verifyIdToken(idToken);
+      req.decodeEmail = decodeUser.email;
+    } catch {}
+  }
+  next();
+}
+
+async function run() {
+  try {
+    await client.connect();
+    console.log("hitting data base");
+    const database = client.db("doctors");
+    const appointmentsCollection = database.collection("appointments");
+    const usersCollection = database.collection("users");
+
+    app.get("/appointments", verifyToken, async (req, res) => {
+      const email = req.query.email;
+      const date = new Date(req.query.date).toLocaleDateString();
+      const query = { email: email, date: date };
+      const cursor = appointmentsCollection.find(query);
+      const appointments = await cursor.toArray();
+      res.json(appointments);
+    });
+
+    app.post("/appointments", async (req, res) => {
+      const appointments = req.body;
+      // console.log(appointments);
+      // res.json({ message: "hellow" });
+      const result = await appointmentsCollection.insertOne(appointments);
+      console.log(result);
+      res.json(result);
+    });
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const result = await usersCollection.insertOne(user);
+      res.json(result);
+    });
+    app.get("/users/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      let isAdmin = false;
+      if (user?.role === "admin") {
+        isAdmin = true;
+      }
+      res.json({ admin: isAdmin });
+    });
+    app.put("/users", async (req, res) => {
+      const user = req.body;
+      const filter = { email: user.email };
+      const options = { upsert: true };
+      const updateDoc = { $set: user };
+      const result = await usersCollection.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
+      res.json(result);
+    });
+    app.put("/users/admin", verifyToken, async (req, res) => {
+      const user = req.body;
+      const requester = req.decodeEmail;
+      if (requester) {
+        const requesterAccoutn = await usersCollection.findOne({
+          email: requester,
+        });
+        if (requesterAccoutn.role === "admin") {
+          const filter = { email: user.email };
+          const updateDoc = { $set: { role: "admin" } };
+          const result = await usersCollection.updateOne(filter, updateDoc);
+          res.json(result);
+        }
+      } else {
+        res
+          .status(403)
+          .json({ message: "You do not have access to make admin" });
+      }
+    });
+  } finally {
+    // await client.close();
+  }
+}
+
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Running Doctors Portal Server");
+});
+
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`);
+});
