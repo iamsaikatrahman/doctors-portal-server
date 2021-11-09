fde045fdeb4fe4003f1ec8815192c0262f02f84b
iamsaikatrahman <saikatrahmanoffical@gmail.com>
2021-11-09 16:40:16 +0600

    update

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ const ObjectId = require("mongodb").ObjectId;
 const app = express();
 const port = process.env.PORT || 5000;
 
-// const serviceAccount = require("./doctors-portal-firebase-adminsdk.json");
 const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
 admin.initializeApp({
